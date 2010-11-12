d578fe4ef3a775e2d1db7003a7b98e0b9d5ef16a
John Clem <johnnyclem@gmail.com>
2010-11-11 16:11:18 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoHaiku::Application.config.secret_token = 'f258a988f21b9c2750dba04f990cf970e02bb5d040b32e442095993dbeb9b06d53930c0a02f6e4b3994a9acbebd67829fdca2604b26beef2c21ba48a11364887'
